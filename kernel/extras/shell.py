c6524f660325d2d456d7ac8b8ce72d0e8ca34d8f
Kai D. Gonzalez <gkai70264@gmail.com>
2021-10-15 07:34:24 -0700

    Change the shell.py to use kernel.current instead of os.

--- kernel/extras/shell.py
+++ kernel/extras/shell.py
@@ -4,6 +4,7 @@ import kernel.http as http
 import kernel.extras.shellinterpret as interp
 import kernel.filesystem as kfs
 import kernel.security.mask as mask
+import kernel.current as cur
 import os
 import kernel.reader as kreader
 import kernel.extras.toolkit as tk
@@ -11,7 +12,7 @@ import kernel.extras.toolkit as tk
 def tmain():
     while (True):
         try:
-            line = kreader.read(mask.pure_user() + "@" + os.getcwd() + "$ ")
+            line = kreader.read(mask.pure_user() + "@" + cur.WorkingDir() + "$ ")
             interp.process(line)
         except KeyboardInterrupt:
             print("logout")

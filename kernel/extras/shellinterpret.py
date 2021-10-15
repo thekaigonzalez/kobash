14ae44bfa0df2f82e3344edf5f386231aa60d589
Kai D. Gonzalez <gkai70264@gmail.com>
2021-10-15 09:56:15 -0700

    Add backwards compatibility with `sh`

--- kernel/extras/shellinterpret.py
+++ kernel/extras/shellinterpret.py
@@ -12,44 +12,59 @@ import kernel.processor as proc
 import kernel.ecosystem as eco
 import kernel.path as kp
 
-# process string
-def process(stri: str):
-    keys = stri.split(" ")
-    if keys[0] == "cd":
-        if (len(keys) < 2):
-            print("cd: expected an argument")
-        else:
-            os.chdir(keys[1])
-    elif keys[0] == "mount":
-        if len(keys) <= 2:
-            print("usage: mount <disk> <dest> <options>")
-            print('''
-the MOUNT utility is a builtin system utility written in Python.
-It's a direct call to the `mount` syscall on unix.
+# aliases change the command to the command it's binded to.
+aliases = {
+    'help': 'pygo'
+}
 
-This command isn't supported on Windows, but instead of error-ing, it'll just not do anything. Sorry!
-            ''')
-    
-        else:
-            libc.mount(keys[1], keys[2], keys[3], keys[4] if len(keys) > 3 else "")
-    elif keys[0] == "ls":
-        try:
-            if (len(keys) <= 1):
-                print(" ".join(os.listdir(".")))
+# process string
+def process(strit: str):
+    for stri in strit.split(";"):
+        keys = stri.split(" ")
+        if keys[0] in aliases:
+            
+            keys[0] = aliases[keys[0]]
+        if keys[0] == "cd":
+            if (len(keys) < 2):
+                print("cd: expected an argument")
             else:
-                print(" ".join(os.listdir(" ".join(keys[1:]))))
-        except FileNotFoundError:
-            print("ls: No such file or directory, '" + " ".join(keys[1:]) + "'")
-    elif keys[0] == "clear":
-        if proc.is_windows:
-            os.system("cls")
+                os.chdir(keys[1])
+        elif keys[0] == "mount":
+            if len(keys) <= 2:
+                print("usage: mount <disk> <dest> <options>")
+                print('''
+    the MOUNT utility is a builtin system utility written in Python.
+    It's a direct call to the `mount` syscall on unix.
+
+    This command isn't supported on Windows, but instead of error-ing, it'll just not do anything. Sorry!
+                ''')
+        
+            else:
+                libc.mount(keys[1], keys[2], keys[3], keys[4] if len(keys) > 3 else "")
+        elif keys[0] == "ls":
+            try:
+                if (len(keys) <= 1):
+                    print(" ".join(os.listdir(".")))
+                else:
+                    print(" ".join(os.listdir(" ".join(keys[1:]))))
+            except FileNotFoundError:
+                print("ls: No such file or directory, '" + " ".join(keys[1:]) + "'")
+        elif keys[0] == "alias":
+            print("added " + keys[0] + " to aliases.")
+            aliases[keys[1]] = "".join(keys[2:])
+            print(aliases)
+        elif keys[0] == "clear":
+            if proc.is_windows:
+                os.system("cls")
+            else:
+                os.system("clear")
         else:
-            os.system("clear")
-    else:
-        if (kp.exists(keys[0])) and kp.isdir(keys[0]):
-            eco.load_module(keys[0], keys[1:])
-        if (kp.exists("/usr/share/kobash/" + keys[0])) and kp.isdir(keys[0]):
-            eco.load_module("/usr/share/kobash/" + keys[0], keys[1:])
-        else:
-            print("kobash: there isn't an ecosystem, builtin, or any file with the name, '{}'".format(keys[0]))
+            if (keys[0] != "./") or keys[0] != ".":
+                if (kp.exists(keys[0])) and kp.isdir(keys[0]):
+                    eco.load_module(keys[0], keys[1:])
+                if (kp.exists("/usr/share/kobash/" + keys[0])) and kp.isdir("/usr/share/kobash/" + keys[0]):
+                    eco.load_module("/usr/share/kobash/" + keys[0], keys[1:])
+                else:
+                    if os.system(" ".join(keys)) != 0:
+                        print("kobash: there isn't an ecosystem, builtin, or any file with the name, '{}'".format(keys[0]))
 

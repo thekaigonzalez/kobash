5d56ee2d4f60698db3ff90ce35e1412575978a1a
Kai D. Gonzalez <gkai70264@gmail.com>
2021-10-15 07:46:21 -0700

    Update toolkit to now support custom ecosystems

--- kernel/extras/toolkit.py
+++ kernel/extras/toolkit.py
@@ -2,6 +2,25 @@ import os
 import sys
 import pathlib
 
+class EcoSystem:
+    # name=The name of your ecosystem.
+    # ps1= the prompt. Like the unix PS1 variable.
+    def __init__(self, name, ps1) -> None:
+        self.name = name
+        self.prompt = ps1
+    def Initialize(self):
+        self.Init()
+'''
+myeco = EcoSystem("pygo", "-# ")
+
+def MyEvent():
+    print("hello, ecosystem!")
+
+# Bind "Init" to my custom event.
+myeco.Init = MyEvent
+
+'''
+
 def load():
     if pathlib.Path("handler.py").exists():
         import handler

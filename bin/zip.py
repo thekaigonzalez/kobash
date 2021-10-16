8f6e0e4d0294ef27847ef60df6044c6015143d74
Kai D. Gonzalez <gkai70264@gmail.com>
2021-10-15 19:45:53 -0700

    Fix ports

--- bin/zip.py
+++ bin/zip.py
@@ -8,13 +8,20 @@ import zipfile
 
 # Modified version of zip.py in KTerminal.
 def Main(argv):
-    parser = argparser.ArgumentParser("zip")
-    parser.add_argument("-O", "-output", default="a.zip")
-    global arg
-    arg = parser.parse_args(argv)
+    if len(argv) < 1:
+        print("zip: option required\nzip: usage: zip [-h] [FILE ...]")
+        return 2
+    if (argv[0] == "-h"):
+        print("zip: usage: zip [-h] [FILE ...]")
+    else:
+        if len(argv) <= 1:
+            print("zip: missing argument\nzip: usage: zip [-h] [FILE ...]")
+            return 1; # kobash has return handlers. pretty sick!
+        if len(argv) == 2:
+            fn = zipfile.ZipFile(argv[0], 'w')
+            fn.write(argv[1])
+            fn.close()
+            return 0; # kobash has return handlers. pretty sick!
+        
     
-    """ ZIP FILE TEST """
-    # fn = zipfile.ZipFile(arg.output, 'w')
-    # fn.write("./usr/bin/cat.py")
-    # fn.close()
     return 0
